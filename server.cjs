5ee2a09993140df7f087fdec7894500674cac181
JeremyKwok <JeremyKaku@outlook.com>
2024-04-26 06:22:51 -0700

    Changed http

--- server.cjs
+++ server.cjs
@@ -9,8 +9,8 @@ const process = require("process");
 
 const app = express();
 
-const mongoDBEndpoint =
-  // "mongodb+srv://jing:986689@webdev.rporsug.mongodb.net/?retryWrites=true&w=majority&appName=WebDev";
+// const mongoDBEndpoint =
+// "mongodb+srv://jing:986689@webdev.rporsug.mongodb.net/?retryWrites=true&w=majority&appName=WebDev";
 
 const mongoDBEndpoint = process.env.MONGODB_URI || "mongodb://127.0.0.1/WebDev";
 
